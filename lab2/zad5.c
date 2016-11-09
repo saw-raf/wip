7a8f3a0195f8e3a838e1aec311b926f3891e696a
raf.station <rafals0707@gmail.com>
2016-11-09 01:55:17 +0100

    modified zad5 - is ready - & compiled it

--- lab2/zad5.c
+++ lab2/zad5.c
@@ -1,29 +1,16 @@
 #include <stdio.h>
 #include <math.h>
 
-double root(double, int);
+
 
 int main()
 {
-	printf("%lf\n",root(2,2));
-	return 0;
-}
+	int i;
+	double wynik=1;
 
-double root(double number, int n)
-{
-	double result=number;
-	double tmp=pow(result,(n-1));
-	double e=0.000001;
-	double checker=number-result*tmp;
-	if(checker<0)
-		checker=-checker;
-	while(checker>=e)
-	{
-		result=1/n*((n-1)*result+(number/tmp));
-		tmp=pow(result,(n-1));
-		checker=number-result*tmp;
-		if(checker<0)
-			checker=-checker;
-	}
-	return result;
+	puts("Program policzy pierwiastek 1000 stopnia z liczby 1000!");
+	for(i=1000;i>=2;i--)
+		wynik*=pow(i,(1.0/1000));
+	printf("Wynik to %0.14lf\n",wynik);
+	return 0;
 }
